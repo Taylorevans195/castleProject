e4752349b87845d01ec1f0c6af7f5a5c2c1d18c8
Taylor Evans <taylorevans195@gmail.com>
2023-04-26 12:34:41 -0400

    ready

---
+++ public/home.js
@@ -0,0 +1,5 @@
+const castle = document.querySelector('img')
+
+castle.addEventListener('click',(evt)=>{
+    alert('We have been expecting you')
+})
---
+++ public/profile.js
@@ -0,0 +1,5 @@
+const myName = document.querySelector('h1')
+
+myName.addEventListener('click',(evt) => {
+    alert('Yo thanks for coming by this is where its at')
+})
